7fcfd42cbd6c57e2e5e99e0dc619806a70bfa97f
Keegan Evans <keegan.evans@gmail.com>
2021-11-11 11:07:49 -0700

    added on_failure workflow

--- the_cure/tests/test_basics.py
+++ the_cure/tests/test_basics.py
@@ -6,5 +6,5 @@ class TestBasics(unittest.TestCase):
     def test_robert(self):
         self.assertEqual("hello robert smith", hello_robert_smith())
 
-    #def test_not_robert(self):
-    #    self.assertEqual("hello michael dempsey", hello_robert_smith())
+    def test_not_robert(self):
+        self.assertEqual("hello michael dempsey", hello_robert_smith())
